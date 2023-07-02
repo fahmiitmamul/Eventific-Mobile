9db006424eeaf648d3a0a4269293926719e8fd90
fahmipraga <itmamul2004@gmail.com>
2023-07-02 19:40:51 +0700

    fix: search event

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -88,6 +88,7 @@ const Home = ({navigation}) => {
               placeholderTextColor="white"
               onChangeText={ev => setSearch(ev)}
               onSubmitEditing={() => getEventByName(search)}
+              onBlur={() => getEventByName()}
             />
             <FontAwesomeIcon
               icon={faSearch}
