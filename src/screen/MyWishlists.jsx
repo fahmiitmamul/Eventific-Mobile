68d892e8c16595066f9a79d73fb779b447aafa5b
fahmipraga <itmamul2004@gmail.com>
2023-07-12 13:59:15 +0700

    fix: replace mapped item with flatlist

--- src/screen/MyWishlists.jsx
+++ src/screen/MyWishlists.jsx
@@ -1,28 +1,45 @@
-import {Appbar} from 'react-native-paper';
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, Touchable} from 'react-native';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faXmark} from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 import styles from '../styles/global';
-import HamburgerIcon from '../assets/images/hamburger.png';
-import {useFocusEffect} from '@react-navigation/native';
 import http from '../helpers/http';
-import {useSelector} from 'react-redux';
 import moment from 'moment';
-import {ScrollView} from 'react-native-gesture-handler';
-import {Modal, Pressable} from 'react-native';
+import {Appbar} from 'react-native-paper';
+import {View, Text, FlatList} from 'react-native';
+import {useFocusEffect} from '@react-navigation/native';
+import {useSelector} from 'react-redux';
+
+const WishlistData = ({item}) => {
+  return (
+    <View style={styles.ManageWrapperStyle}>
+      <View style={styles.ManageWrapperChildStyle}>
+        <View style={styles.DateWrapper}>
+          <Text style={styles.TextDate}>{moment(item.date).format('DD')}</Text>
+          <Text style={styles.FontStyle}>
+            {moment(item.date).format('ddd')}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.TitleWrapper}>
+        <View>
+          <Text style={styles.TitleStyles}>{item.title}</Text>
+        </View>
+        <View>
+          <View>
+            <Text style={styles.FontStyle}>{item.location}</Text>
+          </View>
+          <View>
+            <Text style={styles.FontStyle}>
+              {moment(item.date).format('LLL')}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const MyWishlists = ({navigation}) => {
-  const [wishlist, setWishlist] = useState([]);
   const token = useSelector(state => state.auth.token);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [deleteWishlist, setDeleteWishlist] = useState([]);
-
-  function openModal(itemId) {
-    const wishlistId = itemId;
-    setModalVisible(!modalVisible);
-    setDeleteWishlist(wishlistId);
-  }
+  const [wishlist, setWishlist] = React.useState([]);
 
   useFocusEffect(
     React.useCallback(() => {
@@ -53,57 +70,17 @@ const MyWishlists = ({navigation}) => {
           title="My Wishlists"
         />
       </Appbar.Header>
-      <ScrollView
+      <FlatList
         style={{
           backgroundColor: 'white',
           borderTopLeftRadius: 40,
           borderTopRightRadius: 40,
-        }}>
-        {wishlist.map(w => {
-          return (
-            <View style={styles.ManageWrapperStyle} key={w.id}>
-              <View style={styles.ManageWrapperChildStyle}>
-                <View style={styles.DateWrapper}>
-                  <Text style={styles.TextDate}>
-                    {moment(w.date).format('DD')}
-                  </Text>
-                  <Text style={styles.FontStyle}>
-                    {moment(w.date).format('ddd')}
-                  </Text>
-                </View>
-              </View>
-              <View style={styles.TitleWrapper}>
-                <View>
-                  <Text style={styles.TitleStyles}>{w.title}</Text>
-                </View>
-                <View>
-                  <View>
-                    <Text style={styles.FontStyle}>{w.location}</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.FontStyle}>
-                      {moment(w.date).format('LLL')}
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </View>
-          );
-        })}
-        {wishlist.length <= 0 && (
-          <View>
-            <Text
-              style={{
-                fontFamily: 'Poppins-Medium',
-                fontSize: 20,
-                textAlign: 'center',
-                marginTop: 20,
-              }}>
-              No wishlist found
-            </Text>
-          </View>
-        )}
-      </ScrollView>
+        }}
+        data={wishlist}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => {
+          return <WishlistData item={item} />;
+        }}></FlatList>
     </View>
   );
 };
