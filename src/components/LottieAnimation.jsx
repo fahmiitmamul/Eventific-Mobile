8da4ffebfa374eb09834168e6334eb5c2c718a07
fahmipraga <itmamul2004@gmail.com>
2023-07-16 21:18:45 +0700

    fix: spacing

--- src/components/LottieAnimation.jsx
+++ src/components/LottieAnimation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import LottieView from 'lottie-react-native';
+
 export default function SimpleLottie() {
   return (
     <View>
