d57e623af64c9d5eefca98b60cd9eddd6ee4eeff
fahmipraga <itmamul2004@gmail.com>
2023-07-21 16:03:00 +0700

    fix: rename application

--- ios/EventificTests/EventificTests.m
+++ ios/EventificTests/EventificTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface eventpragaTests : XCTestCase
+@interface EventificTests : XCTestCase
 
 @end
 
-@implementation eventpragaTests
+@implementation EventificTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
